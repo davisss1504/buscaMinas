0f95cde15da6f2d2c27c5fa6a21a03c083aafaf2
kencruga14 <kag.cruz@yavirac.edu.ec>
2020-08-12 08:56:02 -0500

    arreglo de controlador y redireccion

--- controlador/autenticador.js
+++ controlador/autenticador.js
@@ -1,5 +1,7 @@
+
 function registro(){ 
     var usuario = document.getElementById("nombre").value   
     sessionStorage.setItem('usuario', usuario);
    window.location.replace("http://localhost/buscaminas/vista/juego.html");  
 }
+
--- controlador/tablero.js
+++ controlador/tablero.js
@@ -24,6 +24,7 @@ function reiniciar() {
   crearTablero(FILAS, COLUMNAS);
 }
 
+
 function terminado(ganador) {
   let mensaje = null;
   let icono = null;
